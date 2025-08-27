34fbb1a20a561d1b4bbf3821cd2abd046ae157e7
Architha <168562220+ArchithaRao@users.noreply.github.com>
2025-08-27 10:29:09 +0530

    Add files via upload

---
+++ color_picker/script.js
@@ -0,0 +1,16 @@
+document.body.style.backgroundColor="pink"
+//  function redColor(){
+//     document.body.style.backgroundColor="red"
+// }
+// function greenColor(){
+//     document.body.style.backgroundColor="green"
+// }
+// function blueColor(){
+//     document.body.style.backgroundColor="blue"
+// }
+// function orangeColor(){
+//     document.body.style.backgroundColor="orange"
+// }
+function onClickChange(color){
+    document.body.style.backgroundColor=color
+}
